217d639b9dfd2eb373c3d734578ebd53bd901fbc
Project-Hope <project4hopes@gmail.com>
2025-10-27 16:04:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
    */
   function setupCountdown() {
     // SET YOUR CAMPAIGN END DATE HERE
-    const campaignEndDate = new Date("February 3, 2026 12:00:00").getTime();
+    const campaignEndDate = new Date("February 8, 2026 01:00:00").getTime();
 
     // Get elements
     const daysEl = document.getElementById("countdown-days");
@@ -158,3 +158,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 3000); // Show for 3 seconds
   }
 });
+
